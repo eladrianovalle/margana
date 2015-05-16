a7d4b65134a25c008036e44c95b9385924c63830
Adriano Valle <adrianovalle@mac.com>
2015-05-16 13:09:19 -0400

    fixed method declaration

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -13,36 +13,36 @@ post '/' do
 
   word = @word
 
-  # def three_letters?(word)
-  #   if word.length == 3
-  #     true
-  #   else
-  #     false
-  #   end
-  # end 
-
-  # def disctinct_letters?(word)
-  #   letter_array = word.chars
-  #   unique_letters = letter_array.uniq
-  #   if unique_letters.length < letter_array.length
-  #     false
-  #   else
-  #     true
-  #   end
-  # end
-
-  # def valid_input?(word)
-  #   if three_letters?(word) && disctinct_letters?(word)
-  #     true
-  #   else
-  #     false
-  #   end
-  # end
-
-  if Word.valid_input?(word)
+  if valid_input?(word)
     redirect "/anagrams/#{word}"
   else
     @error = "Error.  I'll need a 3 letter word."
     erb :index
   end
 end
+
+  def three_letters?(word)
+    if word.length == 3
+      true
+    else
+      false
+    end
+  end 
+
+  def disctinct_letters?(word)
+    letter_array = word.chars
+    unique_letters = letter_array.uniq
+    if unique_letters.length < letter_array.length
+      false
+    else
+      true
+    end
+  end
+
+  def valid_input?(word)
+    if three_letters?(word) && disctinct_letters?(word)
+      true
+    else
+      false
+    end
+  end
