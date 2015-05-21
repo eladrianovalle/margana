b053d95af1a8cc6a5c9060af752a6a8633319512
Adriano Valle <adrianovalle@mac.com>
2015-05-21 11:48:31 -0400

    added add new word view

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -3,6 +3,17 @@ get '/words' do
   erb :"/words/index"
 end
 
+get '/words/new' do
+  @word = Word.new
+  erb :"/words/new"
+end
+
+post '/words' do
+  @word = Word.new(text: params[:text])
+  @word.save
+  redirect "/words/#{@word.id}"
+end
+
 get '/words/:id' do
   @word = Word.find(params[:id])
   erb :"words/show"
